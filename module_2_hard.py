46702a32520f0904cd192768961e4149da7b2cb2
Tetewa <tetewa1369@gmail.com>
2024-11-14 13:49:39 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -4,7 +4,7 @@ def generate_password(n):
     result = ""
     for i in range(1, n):
         for j in range(i + 1, n): 
-            if (i + j) % n == 0:
+            if n % (i + j) == 0:
                 result += str(i) + str(j)              
     return result
 n = random.randint(3, 20)
